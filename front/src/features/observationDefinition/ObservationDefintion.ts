3e518570f7882ee28c98b0f0cfac289847364d1c
kobebrylant <wojto811@wp.pl>
2026-05-30 11:25:58 +0200

    Fixes

--- front/src/features/observationDefinition/ObservationDefintion.ts
+++ front/src/features/observationDefinition/ObservationDefintion.ts
@@ -5,6 +5,10 @@ export class ObservationDefinition {
   preferredReportName: string;
   code: string | null;
   codeDisplay: string | null;
+  methodCode: string | null;
+  methodDisplay: string | null;
+  permittedUnitCode: string | null;
+  permittedUnitDisplay: string | null;
 
   constructor(data: ObservationDefinitionResource) {
     this.id = data.id;
@@ -12,5 +16,9 @@ export class ObservationDefinition {
       data.preferredReportName || "Brak nazwy raportowej";
     this.code = data.code?.coding?.[0]?.code ?? null;
     this.codeDisplay = data.code?.coding?.[0]?.display ?? null;
+    this.methodCode = data.method?.coding?.[0]?.code ?? null;
+    this.methodDisplay = data.method?.coding?.[0]?.display?.trim() ?? null;
+    this.permittedUnitCode = data.permittedUnit?.[0]?.code ?? null;
+    this.permittedUnitDisplay = data.permittedUnit?.[0]?.display ?? null;
   }
 }
